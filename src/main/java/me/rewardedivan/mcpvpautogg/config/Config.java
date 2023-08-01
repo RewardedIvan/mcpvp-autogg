a23d4148b63687519f4d4c978762b3f4b37ea079
RewardedIvan <RewardedIvan@gmail.com>
2023-08-02 00:35:16 +0300

    Initial commit

---
+++ src/main/java/me/rewardedivan/mcpvpautogg/config/Config.java
@@ -0,0 +1,2 @@
+package me.rewardedivan.mcpvpautogg.config;public class Config {
+}
---
+++ src/main/java/me/rewardedivan/mcpvpautogg/mixin/ChatHudMixin.java
@@ -0,0 +1,8 @@
+package me.rewardedivan.mcpvpautogg.mixin;
+
+import net.minecraft.client.gui.hud.ChatHud;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ChatHud.class)
+public class ChatHudMixin {
+}
